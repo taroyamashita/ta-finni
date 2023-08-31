1481c44f820abd6207e2e74e5d2372d715c0004b
taroyamashita <taro.c.yamashita@gmail.com>
2023-08-31 17:22:19 -0400

    adds sorting

--- client/src/Components/Form/CustomForm.tsx
+++ client/src/Components/Form/CustomForm.tsx
@@ -1,59 +1,136 @@
-import { FieldValue, collection, doc, setDoc, onSnapshot } from 'firebase/firestore';
+import { collection, doc, setDoc, onSnapshot } from 'firebase/firestore';
 import React, { useEffect, useState } from 'react';
 import { app, db } from '../../firebase';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { getAuth } from 'firebase/auth';
-import { useForm } from 'react-hook-form';
+import { Controller, useForm } from 'react-hook-form';
 import { v4 as uuidv4 } from 'uuid';
-type FormData = {
-    age: string;
-    name: string;
-  };
+import './CustomForm.css';
+
+
+
+export interface FormTemplate extends Record<string, any> {
+  id: string;
+  fields: InputField[];
+}
+
+
+type InputField = {
+  name: string;
+  label: string;
+  isRequired: boolean;
+}
+
+// InputFieldProps.ts
+type InputFieldProps = {
+  name: string;
+  label: string;
+  type?: 'text' | 'number' | 'email' | 'password'; // Add any additional types you need
+};
+
+// DynamicFormData.ts
+type DynamicFormData = {
+  [key: string]: any;
+};
+
+interface DynamicFormProps {
+  fields: InputFieldProps[];
+  onSubmit: (data: DynamicFormData) => Promise<void>;
+}
+
+const DynamicForm: React.FC<DynamicFormProps> = ({ fields, onSubmit }) => {
+  const { handleSubmit, control, reset } = useForm<DynamicFormData>();
+
+  const onSubmitAndReset = async (data: DynamicFormData) => {
+    await onSubmit(data);
+    reset();
+  }
+
+  return (
+    <form onSubmit={handleSubmit(onSubmitAndReset)}>
+      {fields.map((field, index) => (
+        <div key={index}>
+          <label>{field.label}</label>
+          <Controller
+            name={field.name}
+            control={control}
+            render={({ field }) => <input {...field} type={'text'} />}
+          />
+        </div>
+      ))}
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
 
 
 
 export function CustomForm() {
   const auth = getAuth(app);
   const [user] = useAuthState(auth)
+
+  const [formTemplate, setFormTemplate] = useState<FormTemplate>({id: '', fields: []});
+
+  useEffect(() => {
+    // get all the forms
+    const unsubscribe = onSnapshot(collection(db, "forms"), (snapshot) => {
+      const newData: FormTemplate[] = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data()
+      }as FormTemplate));
+      setFormTemplate(newData[0]);
+    });
+
+    return () => {
+      unsubscribe()
+    }
+  }, []);
+
   // logic for adding, removing, reordering form fields
   
-  const saveForm = async () => {
-    // logic for saving form to Firestore
-    await setDoc(doc(db, "forms", "myform"), {
-        name: "Los Angeles",
-        state: "CA",
-        country: "USA",
+  const saveFormTemplate = async (formTemplate: FormTemplate) => {
+    try {
+       // logic for saving form to Firestore
+      await setDoc(doc(db, "forms", formTemplate.id), {
+        name:  "Untitled Form",
+        fields: formTemplate.fields,
         uid: user!.uid
       });
+    } catch (e) {
+      console.log(e)
+    }
+   
   };
 
-  const saveToFirebase = async (data: FormData) => {
-    const { age, name } = data;
+  const savePatientData = async (data: DynamicFormData) => {
     try {
       await setDoc(doc(db, "patientData", uuidv4() ), {
-        age,
-        name,
+        ...data,
         uid: user!.uid,
       });
     } catch (error) {
       console.error('Error writing document: ', error);
     }
-    reset();
   };
 
+  const addFormField = () => {
+    const name = prompt("add form field name");
+    if(!name) return;
+    setFormTemplate((prev) => ({
+      ...prev,
+      fields: [...prev.fields, {name, label: name, isRequired: false}]
+    }));
+  }
 
-  const { register, setValue, handleSubmit, formState: { errors }, reset } = useForm<FormData>();
-  const onSubmit = handleSubmit(data => saveToFirebase(data));
-  // firstName and lastName will have correct type
 
   return (
-    <form onSubmit={onSubmit}>
-      <label>Name</label>
-      <input {...register("name")} />
-      <label>Age</label>
-      <input {...register("age")} />
-      <button type="submit">Submit</button>
-    </form>
-  );
-    
+    <>
+      <DynamicForm fields={formTemplate.fields} onSubmit={savePatientData} />
+      <button onClick={addFormField}>Add form field</button>
+      <button onClick={() => {
+        saveFormTemplate(formTemplate)
+      }}>Save Form</button>
+
+    </>
+  );   
 }
--- client/src/Components/Table/CustomTable.tsx
+++ client/src/Components/Table/CustomTable.tsx
@@ -1,39 +1,100 @@
-import { Table, TableHead, TableRow, TableCell, TableBody } from "@mui/material";
+import React, { useEffect, useState } from 'react';
+import { Table, TableHead, TableRow, TableCell, TableBody, TableSortLabel } from "@mui/material";
+import { onSnapshot, collection } from "@firebase/firestore";
+import { db } from "../../firebase";
+import { FormTemplate } from "../Form/CustomForm";
 
 interface Row extends Record<string, any> {
-    id: number;
-    name: string;
-    age: number;
+  id: number;
 }
 
 interface CustomTableProps {
-    patientData: Row[];
+  patientData: Row[];
+}
+
+interface SortConfig {
+  key: string;
+  direction: 'asc' | 'desc';
 }
 
 export const CustomTable: React.FC<CustomTableProps> = (props: CustomTableProps) => {
 
-    const { patientData } = props;
-    
-    return(
-        <div style={{ padding: '20px' }}>
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell>ID</TableCell>
-              <TableCell>Name</TableCell>
-              <TableCell>Age</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {patientData.map((row) => (
-              <TableRow key={row.id}>
-                <TableCell>{row.id}</TableCell>
-                <TableCell>{row.name}</TableCell>
-                <TableCell>{row.age}</TableCell>
-              </TableRow>
+  const { patientData } = props;
+  const [keys, setKeys] = useState<string[]>([]);
+  const [sortConfig, setSortConfig] = useState<SortConfig | null>(null);
+
+  const sortedData = React.useMemo(() => {
+    let sortableData = [...patientData];
+    if (sortConfig !== null) {
+      sortableData.sort((a, b) => {
+        if (a[sortConfig.key] < b[sortConfig.key]) {
+          return sortConfig.direction === 'asc' ? -1 : 1;
+        }
+        if (a[sortConfig.key] > b[sortConfig.key]) {
+          return sortConfig.direction === 'asc' ? 1 : -1;
+        }
+        return 0;
+      });
+    }
+    return sortableData;
+  }, [patientData, sortConfig]);
+
+  const requestSort = (key: string) => {
+    const direction = 
+      sortConfig && sortConfig.key === key && sortConfig.direction === 'asc'
+        ? 'desc'
+        : 'asc';
+    setSortConfig({ key, direction });
+  };
+
+  useEffect(() => {
+    const unsubscribe = onSnapshot(collection(db, "forms"), (snapshot) => {
+      const newData: FormTemplate[] = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      } as FormTemplate));
+      const keys = [...newData[0].fields.map(field => field.name), 'id'];
+      setKeys(keys);
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  if (patientData.length === 0) {
+    return <div>No data available</div>;
+  }
+
+  return (
+    <div style={{ padding: '20px' }}>
+      <Table>
+        <TableHead>
+          <TableRow>
+            {['ID', ...keys].map((key, index) => (
+              <TableCell key={index}>
+                <TableSortLabel
+                  active={sortConfig ? sortConfig.key === key : false}
+                  direction={sortConfig ? sortConfig.direction : 'asc'}
+                  onClick={() => requestSort(key)}
+                >
+                  {key}
+                </TableSortLabel>
+              </TableCell>
             ))}
-          </TableBody>
-        </Table>
-      </div>
-    )
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {sortedData.map((row) => (
+            <TableRow key={row.id}>
+              <TableCell>{row.id}</TableCell>
+              {keys.map((key, index) => (
+                <TableCell key={index}>{row[key] || ''}</TableCell>
+              ))}
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </div>
+  );
 }
